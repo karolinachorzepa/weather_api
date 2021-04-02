3f013c52fccfdbc5a1703c9a1317f5d33858d35b
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2021-04-02 20:30:04 +0200

    library appended

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,4 +1,5 @@
 import requests, json
+from bs4 import BeautifulSoup
 
 class Temperature(object):
     def __init__(self,place):
@@ -9,8 +10,10 @@ class Temperature(object):
         return str(f"It's currently{temperature} in {self.place}")
 
     def weather(self):
-        url=f"https://openweathermap.org/" in {self.place}
+        url = f"https://duckduckgo.com/?q=weather&t=chromentp&ia=weather in {self.place}"
         req=requests.get(url)
+        scrape = BeautifulSoup (req.text, "html.parser")
+        temperature = scrape.find("div", class_="BNeawe").text
         return temperature
 
 if __name__=="__main__":
